baf6264812bdead7ff7ddb1014e9046088244a15
Sergey <sergios33033@gmail.com>
2021-07-12 17:07:54 +0300

    The ThrottlerGuard removed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
-import { Module } from "@nestjs/common";
-import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule } from "@nestjs/config";
-import { APP_GUARD } from "@nestjs/core";
-import { ValidationModule } from "./pipes/validation.module";
+import { Module } from "@nestjs/common";
 import { UserController } from "./user/user.controller";
 import { ClientModule } from "./client/client.module";
-import { AuthModule } from "./auth/auth.module";
+import { TokenModule } from "./token/token.module";
 import { UserModule } from "./user/user.module";
 
 @Module({
@@ -15,24 +12,10 @@ import { UserModule } from "./user/user.module";
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${process.env.MONGO_DATABASE_NAME}?retryWrites=true&w=majority`
     ),
-    ThrottlerModule.forRoot({
-      ttl: 300,
-      limit: 10,
-      ignoreUserAgents: [new RegExp("googlebot", "gi"), new RegExp("bingbot", "gi")]
-    }),
-    ValidationModule,
-    AuthModule,
+    TokenModule,
     UserModule,
     ClientModule
   ],
-  controllers: [UserController],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard
-    }
-  ]
+  controllers: [UserController]
 })
 export class AppModule {}
-
-// TODO public API, REDIS microservices (auth, entrance, client, email verification, rooms), test, push to heroku, FRONT
