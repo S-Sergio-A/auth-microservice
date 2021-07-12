8d3fd0bcbb7f45ae92dd720ce9f46a649e3ef9c9
Sergey <sergios33033@gmail.com>
2021-07-12 17:06:28 +0300

    Unnecessary code removed.

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,9 @@
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-// import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-// import helmet from "helmet";
-import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import "reflect-metadata";
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // app.use(helmet());
-  // app.enableCors({
-  //   origin: [process.env.FRONT_URL],
-  //   credentials: true,
-  //   exposedHeaders: ["Access-Token", "Refresh-Token", "Client-Token", "Country", "Content-Type"],
-  //   methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"]
-  // });
-
-  // const config = new DocumentBuilder()
-  //   .setTitle("Authentication and entrance microservice")
-  //   .setDescription("The cats API description")
-  //   .setVersion("1.0")
-  //   .addTag("authentication, authorization, entrance")
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup("api", app, document);
-
-  // await app.listen(6000);
-
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.REDIS,
     options: {
@@ -33,7 +12,6 @@ async function bootstrap() {
       retryAttempts: 10
     }
   });
-
   app.listen(() => console.log("Microservice is listening"));
 }
 
